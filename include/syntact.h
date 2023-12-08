5ac1ece894e3ca3182ba10972782c0ff3b9a44b5
Bas van der Hoek <bas@hoek.com>
2023-12-08 13:56:45 +0100

    Add c header file

---
+++ include/syntact.h
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+int32_t add(int32_t a, int32_t b);
+
+const char *unicode_from_rust(void);
+
+const char *highlighter(const char *source, const char *extension);
